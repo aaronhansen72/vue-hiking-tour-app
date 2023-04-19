1ffb654265e0b6e9fc11ff7691f1288e981cb383
Steffen Kube <kube@lmu.de>
2023-04-19 16:40:26 +0200

    HTML comments added. code  clean up

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,22 +1,18 @@
 <template>
     <div class="pl-5">
         <h2 class="mb-3">Suche</h2>
-
-
+        <!-- Search Field -->
         <div id="searchbox" class="pr-6 mb-6">
-           
-                <input type="search" @click="emptyFormField" @focus="searchText = null"  v-model="searchText"
-                    placeholder="Suchbegriff eingeben">
-                    <button type="submit" @click="doSearch()" class="button button-search">Go</button> 
-           
+            <input type="search" @click="emptyFormField" @focus="searchText = null" v-model="searchText"
+                placeholder="Suchbegriff eingeben">
+            <button type="submit" @click="doSearch()" class="button button-search">Go</button>
         </div>
-
+        <!-- Search Result -->
         <p class="mt-5 mb-6" v-html="resultInfo"></p>
         <div v-for="tour in searchResultList" :key="tour.id">
             <TourListItem :tour="tour" :filteredTourList="tourlist"></TourListItem>
         </div>
     </div>
-
 </template>
   
 <script>
@@ -45,13 +41,13 @@ export default {
     methods: {
         doSearch() {
             this.searchResultList = [];
-            if (this.searchText !== ""  && this.searchText !== null) {
+            if (this.searchText !== "" && this.searchText !== null) {
                 console.log("doSearch")
-                this.searchResultList = this.getObjects(this.tourlist,"",this.searchText);
+                this.searchResultList = this.getObjects(this.tourlist, "", this.searchText);
                 if (this.searchResultList) {
                     this.resultInfo = this.getResultInfo(this.searchResultList.length);
                 }
-            }  else {
+            } else {
                 this.resultInfo = "Bitte Suchbegriff eingeben";
             }
 
@@ -59,7 +55,7 @@ export default {
         getObjects(obj, key, val) {
             let objects = [];
             for (var i in obj) {
-                if(!Object.prototype.hasOwnProperty.call(obj,i)) continue
+                if (!Object.prototype.hasOwnProperty.call(obj, i)) continue
                 //if (!obj.hasOwnProperty(i)) continue;
                 if (typeof obj[i] == 'object') {
                     objects = objects.concat(this.getObjects(obj[i], key, val));
@@ -78,86 +74,82 @@ export default {
             }
             return objects;
         },
-        getResultInfo(hits)  {
+        getResultInfo(hits) {
             let hitsText = "Ergebnisse";
-            if (hits === 1)  {
+            if (hits === 1) {
                 hitsText = "Ergebnis";
             }
             return hits + " " + hitsText + " für <b>" + this.searchText + "</b>"
         },
-        emptyFormField()  {
+        emptyFormField() {
             this.searchResultList = [];
             this.resultInfo = "";
         }
     },
-    created()  {
+    created() {
         console.log("created");
         this.storeTours.fetchTourlist().then(() => {
-          this.tourlist = this.storeTours.tourlistData;
-        });  
+            this.tourlist = this.storeTours.tourlistData;
+        });
     }
 }
 </script>
 
 <style scoped>
-
-
 #searchbox {
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 auto;
 }
 
-.searchkey  {
- font-size: 3em;
- color: red;
+.searchkey {
+    font-size: 3em;
+    color: red;
 }
 
 input[type="search"] {
-  border: none;
-  whitespace: wrap;
-  border-radius: 5px 5px 5px 5px;
-	background: rgba(233, 233, 233, 0.5294117647);
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
-	padding: 0.35em 0.75em;
-	border: none;
-	font-size: 1.1em;
-	text-decoration: none;
-	line-height: normal;
-	max-height: 3em;
-  
-  flex: 2 0 70%;
+    border: none;
+    whitespace: wrap;
+    border-radius: 5px 5px 5px 5px;
+    background: rgba(233, 233, 233, 0.5294117647);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    padding: 0.35em 0.75em;
+    border: none;
+    font-size: 1.1em;
+    text-decoration: none;
+    line-height: normal;
+    max-height: 3em;
+
+    flex: 2 0 70%;
 }
 
 input[type="search"]:focus {
-  background: #b1a8a8;
-  color: white;
-  letter-spacing: .1em;
-  transition: background 400ms ease-in-out;
-  outline: none;
+    background: #b1a8a8;
+    color: white;
+    letter-spacing: .1em;
+    transition: background 400ms ease-in-out;
+    outline: none;
 }
 
 button {
-  flex: 1 0 20%;
-  
-  max-height: 3em;
-  font-size: 1.1em;
-  letter-spacing: .2em;
-  text-transform: uppercase;
-  border-radius: 0px 5px 5px 0px;
-	background: rgba(153, 51, 0, 0.8196078431);
-  color: white;
-  border: none;
-  padding: 0.35em 0.75em;
-  cursor: pointer;
+    flex: 1 0 20%;
+
+    max-height: 3em;
+    font-size: 1.1em;
+    letter-spacing: .2em;
+    text-transform: uppercase;
+    border-radius: 0px 5px 5px 0px;
+    background: rgba(153, 51, 0, 0.8196078431);
+    color: white;
+    border: none;
+    padding: 0.35em 0.75em;
+    cursor: pointer;
 
 }
+
 button:hover {
-  background: #993300;
+    background: #993300;
 }
-
-
-
 </style>
