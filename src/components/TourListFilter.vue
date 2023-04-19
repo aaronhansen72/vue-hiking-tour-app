738b81a67d644ff8ed78745806a22248402a4ab4
Steffen Kube <kube@lmu.de>
2023-04-19 19:39:49 +0200

    data years: 2023 added

--- src/components/TourListFilter.vue
+++ src/components/TourListFilter.vue
@@ -49,7 +49,7 @@ export default {
             activeDropdown: -1,
             activeDropdownPrev: -1,
             countries: [ 'Deutschland','Italien','Korsika', 'Österreich','Norwegen','Schweiz','Spanien','Slowenien'],
-            years: ['2022','2021','2020','2019','2018','2017','2016','2015','2014'],
+            years: ['2023','2022','2021','2020','2019','2018','2017','2016','2015','2014'],
             currentFilter: ''
         }
     },      
