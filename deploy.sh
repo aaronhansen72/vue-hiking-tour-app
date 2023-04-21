16fbaf0f87bb0c013ae07a4517af4bb8c2ae065a
Steffen Kube <kube@lmu.de>
2023-04-21 14:09:03 +0200

    deployment paths corrected

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 scp dist/css/* p6746745@home17592662.1and1-data.host:wandern.cube-online.de/css/
 scp dist/css/* p6746745@home17592662.1and1-data.host:wandern.cube-online.de/css/
-scp dist/index.html scp dist/css/* p6746745@home17592662.1and1-data.host:wandern.cube-online.de/css/
+scp dist/index.html p6746745@home17592662.1and1-data.host:wandern.cube-online.de/
