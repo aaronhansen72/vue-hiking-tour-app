57981fbf6c91b95c0838cc936efb6fcb976cabab
Steffen Kube <kube@lmu.de>
2023-04-24 08:20:49 +0200

    changed from scp to rsync

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-scp dist/css/* p6746745@home17592662.1and1-data.host:wandern.cube-online.de/css/
-scp dist/js/*.js p6746745@home17592662.1and1-data.host:wandern.cube-online.de/js/
-scp dist/index.html p6746745@home17592662.1and1-data.host:wandern.cube-online.de/
+#!/bin/bash
+rsync -av  --exclude 'contents' --exclude 'data.json' -e "ssh -i ~/.ssh/id_rsa_ionos" dist/ p6746745@home17592662.1and1-data.host:wandern.cube-online.de/
