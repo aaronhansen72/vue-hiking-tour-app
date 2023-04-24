cd97c3dc913956c6934f28291126cd8924562356
Steffen Kube <kube@lmu.de>
2023-04-24 08:53:58 +0200

    change script location from head to body

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //publicPath: '.',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    // change script location from head to body
+    config.plugin('html').tap((args) => {
+        args[0].scriptLoading = 'blocking'
+        // or
+        // args[0].inject = 'body'
+        return args
+    })
+  }
 })
