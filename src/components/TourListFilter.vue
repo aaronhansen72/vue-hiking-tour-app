5fd2bed44d66c1fa995452986e8125a52088426c
Kube, Steffen <kube@lmu.de>
2025-07-26 17:38:28 +0200

    Add 2025 to years in TourListFilter

--- src/components/TourListFilter.vue
+++ src/components/TourListFilter.vue
@@ -49,7 +49,7 @@ export default {
             activeDropdown: -1,
             activeDropdownPrev: -1,
             countries: [ 'Deutschland','England','Italien','Korsika', 'Österreich','Norwegen','Schweiz','Spanien','Slowenien', 'Wales'],
-            years: ['2024','2023','2022','2021','2020','2019','2018','2017','2016','2015','2014'],
+            years: ['2025','2024','2023','2022','2021','2020','2019','2018','2017','2016','2015','2014'],
             currentFilter: ''
         }
     },      
